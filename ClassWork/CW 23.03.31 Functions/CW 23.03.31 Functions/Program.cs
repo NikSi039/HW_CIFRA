1b6c038de0759eed90a8a6323a4ad5ba7c83302f
NikSi039 <sidorova9999@internet.ru>
2023-03-31 19:04:12 +0200

    create CW

---
+++ ClassWork/CW 23.03.31 Functions/CW 23.03.31 Functions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CW_23._03._31_Functions
+{
+    internal class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine ( "Hello, World!" );
+        }
+    }
+}
