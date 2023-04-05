c5d58ffc2c31ddb901fcf3fcf27604bd72535842
NikSi039 <sidorova9999@internet.ru>
2023-04-05 13:12:36 +0200

    add

---
+++ SimpleCode/23.04.05 Massives/2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 4, 6, 2 };
+
+        Insert(ref myArray, -5, 2);// By Index
+
+        AddFirst(ref myArray, -5); // By Start
+
+        AddLast(ref myArray, -5);  // By End
+
+
+        //END PROGRAMM
+
+
+
+
+        void Insert(ref int[] array, int value, int index)
+        {
+            int[] newArray = new int[array.Length + 1];
+
+            newArray[index] = value;
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = index; i < array.Length; i++)
+            {
+                newArray[i + 1] = array[i];
+            }
+
+            array = newArray;
+        }
+
+        void AddFirst(ref int[] array, int value)
+        {
+            Insert(ref array, value, 0);
+        }
+
+        void AddLast(ref int[] array,int value)
+        {
+            Insert(ref array, value, array.Length);
+        }
+    }
+}
+
---
+++ SimpleCode/23.04.05 Massives/3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 4, 6, 2 };
+
+        RemoveAt(ref myArray, 2);
+
+        RemoveFirst(ref myArray);
+
+        RemoveLast(ref myArray);
+
+
+
+        //END PROGRAMM
+
+
+
+        void RemoveAt(ref int[] array, int index)
+        {
+            int[] newArray = new int[array.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = index + 1; i < array.Length; i++)
+            {
+                newArray[i - 1] = array[i];
+            }
+
+            array = newArray;
+        }
+
+        void RemoveFirst(ref int[] array)
+        {
+            RemoveAt(ref array, 0);
+        }
+
+        void RemoveLast(ref int[] array)
+        {
+            RemoveAt(ref array, array.Length - 1);
+        }
+    }
+}
+
