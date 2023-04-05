c1846439f4b9c3283063c17792c53df09fce8166
NikSi039 <sidorova9999@internet.ru>
2023-04-05 18:53:01 +0200

    add

---
+++ ClassWork/CW 23.04.05 Razbor dz/CW 23.04.05 Razbor dz/CW 23.04.05 Razbor dz/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CW_23._04._05_Razbor_dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
