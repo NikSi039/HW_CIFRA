7323f93d7dbbccb6695ccba35e55fae3c79f1ef0
NikSi039 <sidorova9999@internet.ru>
2023-04-07 21:13:52 +0200

    summ

---
+++ ClassWork/CW 23.04.07 Class/CW 23.04.07 Class/CW 23.04.07 Class/Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_23._04._07_Class
+{
+    public class Number
+    {
+        public int Value = 1;
+        public string Text = "None";
+
+        public void ReadValue()
+        {
+            Console.WriteLine("Введите число: ");
+            int input = int.Parse(Console.ReadLine());
+            Value = input;
+        }
+
+        public void ReadText()
+        {
+            Console.WriteLine("Введите текст");
+            Text = Console.ReadLine();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ClassWork/CW 23.04.07 Class/CW 23.04.07 Class/CW 23.04.07 Class/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace CW_23._04._07_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[10];
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = new User();
+                users[i].Create();
+            }
+
+            users[2].Name = "";
+            users[4].Age = 12;
+
+
+
+            PrintUsers();
+            Console.WriteLine("------------------------------------");
+            TrimUsers();
+            users[0].PrintAgeDifferense(users);
+
+            Console.ReadKey();
+
+            //END PROGRAMM
+
+
+            void PrintUsers()
+            {
+                for (int i = 0; i < users.Length; i++)
+                {
+                    Console.Write(users[i].Name + "\t" + users[i].Surname + "\tВозраст:  " + users[i].Age);
+
+                    if (users[i].Validate() == false)
+                    {
+                        Console.Write("   ---- Не корректный");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            void TrimUsers()
+            {
+                for (int i = 0; i < users.Length; i++)
+                {
+                    users[i].Trim();
+                }
+            }
+        }
+    }
+}
