28ee478e9521a3eed8f2d7e01f4029eced65f7da
NikSi039 <sidorova9999@internet.ru>
2023-04-26 12:43:49 +0200

    a

--- ClassWork/CW 23.04.24 Arrays/CW 23.04.24 Arrays/z2/Program.cs
+++ ClassWork/CW 23.04.24 Arrays/CW 23.04.24 Arrays/z2/Program.cs
@@ -6,6 +6,7 @@
         {
             int[,] array = new int[10, 10];
             int[] newArray = new int[10];
+            int[] res = new int[10];
 
             Random random = new Random();
 
@@ -15,23 +16,50 @@
                 {
                     array[i, j] = random.Next(10, 100);
                     Console.Write(array[i, j] + " ");
+                    newArray[j] = array[i, j];
                 }
+
+                res[i] = newArray.Max();
+
                 Console.WriteLine();
             }
 
             Console.WriteLine();
 
+            foreach (int item in res)
+            {
+                Console.Write(item + " ");
+            }
 
+            Console.WriteLine();
+            Console.WriteLine();
 
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int maximum = int.MinValue;
 
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(10, 100);
+                    Console.Write(array[i,j] + " ");
 
+                    if (array[i,j] > maximum)
+                    {
+                        maximum = array[i, j];
+                    }
 
+                    res[i] = maximum;
+                }
 
+                Console.WriteLine();
+            }
 
+            Console.WriteLine();
 
-
-
-
+            foreach (int item in res)
+            {
+                Console.Write(item + " ");
+            }
 
             Console.ReadKey();
         }
