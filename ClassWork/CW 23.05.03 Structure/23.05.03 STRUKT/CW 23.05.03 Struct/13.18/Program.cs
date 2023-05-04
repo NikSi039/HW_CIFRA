d93a13e1f5f4330a3a64aee5dbe9e90c859e4baa
NikSi039 <sidorova9999@internet.ru>
2023-05-04 14:41:35 +0200

    add

--- ClassWork/CW 23.05.03 Structure/23.05.03 STRUKT/CW 23.05.03 Struct/13.18/Program.cs
+++ ClassWork/CW 23.05.03 Structure/23.05.03 STRUKT/CW 23.05.03 Struct/13.18/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _13._18;
+﻿using System.Text;
+
+namespace _13._18;
 class Program
 {
     //13.18. Даны названия 20 стран и частей света,
@@ -7,14 +9,57 @@ class Program
     //находящиеся в Африке или в Азии.
     //В случае положительного ответа напечатать их названия.
 
+    struct Country
+    {
+        public string country;
+        public string partOfTheWorld;
+    }
+
     static void Main(string[] args)
     {
+        Country[] countries = new Country[20];
+        string[] partOfTheWorlds = { "Европа", "Азия", "Африка", "Америка", "Австралия и Океания" };
+
+        Random random = new Random();
+
+        for (int i = 0; i < countries.Length; i++)
+        {
+            countries[i] = new Country();
+            countries[i].country = GetString(random.Next(4, 10));
+            countries[i].partOfTheWorld = partOfTheWorlds[random.Next(0, partOfTheWorlds.Length)];
+
+            Console.WriteLine(countries[i].country + "," + countries[i].partOfTheWorld);
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nСписок стран, находящихся в Африке или в Азии: \n");
+
+        foreach (Country item in countries)
+        {
+            if (item.partOfTheWorld == "Африка" || item.partOfTheWorld == "Азия")
+            {
+                Console.Write(item.country + " | ");
+            }
+        }
+
+        Console.ReadKey();
 
 
+        //END
 
 
+        string GetString(int n)
+        {
+            StringBuilder str = new StringBuilder();
 
+            str.Append((char)random.Next(1040, 1072));
 
+            for (int i = 0; i < n; i++)
+            {
+                str.Append((char)random.Next(1072, 1104));
+            }
 
+            return str.ToString();
+        }
     }
 }
