6f37306876574dbe3fc5e9e4641de4f79a319d95
NikSi039 <sidorova9999@internet.ru>
2023-05-08 20:49:42 +0200

    add

---
+++ ClassWork/CW 23.05.08 DATE TIME/CW 23.05.08 DATE TIME/problems2          str 155 - 2/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace problems2__________str_155___2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("///////////1/////////////////////////");
+
+            //Console.Write("Введите год, месяц, день первой даты: ");
+            //int year1 = int.Parse(Console.ReadLine());
+            //int month1 = int.Parse(Console.ReadLine());
+            //int day1 = int.Parse(Console.ReadLine());
+            //DateTime d1 = new(year1, month1, day1);
+
+            //Console.Write("Введите год, месяц, день второй даты: ");
+            //int year2 = int.Parse(Console.ReadLine());
+            //int month2 = int.Parse(Console.ReadLine());
+            //int day2 = int.Parse(Console.ReadLine());
+            //DateTime d2 = new(year2, month2, day2);
+
+            //if (d1 > d2)
+            //{
+            //    Console.WriteLine($"Дата {d1} больше, чем дата {d2}");
+            //}
+            //else if (d1 < d2)
+            //{
+            //    Console.WriteLine($"Дата {d1} меньше, чем дата {d2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Равны");
+            //}
+
+            //Console.WriteLine(d1.AddDays(1));
+
+
+
+            Console.WriteLine("////////////2///////////////");
+
+
+            Console.Write("Введите время запуска батареи: ");
+            int year = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+            DateTime d = new(year, month, day, hour, minutes, seconds);
+
+            Console.Write("Введите задекларированное время работы: ");
+            int h = int.Parse(Console.ReadLine());
+
+            if (DateTime.Now.Subtract(d).Hours < h)
+            {
+                Console.WriteLine($"Количество часов работы {DateTime.Now.Subtract(d).Hours}, " +
+                    $"до полного разряда {60*h - DateTime.Now.Subtract(d).Minutes} минут");
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
