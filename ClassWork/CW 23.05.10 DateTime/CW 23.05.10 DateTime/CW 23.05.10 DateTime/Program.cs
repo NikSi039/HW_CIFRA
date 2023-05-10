1a7435e45f6e3ba354237eff51cadfd6f5c597b0
NikSi039 <sidorova9999@internet.ru>
2023-05-10 19:33:35 +0200

    CW 23.05.10 DateTie

---
+++ ClassWork/CW 23.05.10 DateTime/CW 23.05.10 DateTime/CW 23.05.10 DateTime/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CW_23._05._10_DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
