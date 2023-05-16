8df00f7db01008764919a35d07a4630e80acb7f6
NikSi039 <sidorova9999@internet.ru>
2023-05-16 10:49:27 +0200

    add

--- ClassWork/CW 23.05.15 FILES/CW 23.05.15 Files/sbornik 15.4/Program.cs
+++ ClassWork/CW 23.05.15 FILES/CW 23.05.15 Files/sbornik 15.4/Program.cs
@@ -11,3 +11,32 @@ using(FileStream f = new FileStream("File.txt",FileMode.OpenOrCreate))
 		await f.WriteAsync(buffer, 0, buffer.Length);
 	}
 }
+
+// sbornik 15.8
+
+using (FileStream f = new FileStream("File.txt",FileMode.Open,FileAccess.Read))
+{
+	byte[] buffer = new byte[f.Length];
+
+	await f.ReadAsync(buffer, 0, buffer.Length);
+
+	string text = Encoding.Default.GetString(buffer);
+
+	Console.WriteLine(text);
+
+	int count = 0;
+
+	for (int i = 0; i < text.Length; i++)
+	{
+		if (text[i] == '\n')
+		{
+			count++;
+		}
+	}
+
+	Console.WriteLine(count);
+
+	Console.ReadKey();
+}
+
+
--- ClassWork/CW 23.05.15 FILES/CW 23.05.15 Files/sbornik 15.5/Program.cs
+++ ClassWork/CW 23.05.15 FILES/CW 23.05.15 Files/sbornik 15.5/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
 
-using (FileStream f = new FileStream ( "File.txt", FileMode.Append ))
+using (FileStream f = new FileStream("File.txt", FileMode.Append))
+{
+    byte[] buffer = Encoding.Default.GetBytes("До свидания, люди!");
+
+    await f.WriteAsync(buffer, 0, buffer.Length);
+}
