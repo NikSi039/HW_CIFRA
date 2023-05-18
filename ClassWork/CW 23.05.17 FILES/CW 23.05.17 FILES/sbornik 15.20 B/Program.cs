b6f5cb278d7b25dd4aaab3a2746d5e96c5cd2a01
NikSi039 <sidorova9999@internet.ru>
2023-05-18 12:23:42 +0200

    end

--- ClassWork/CW 23.05.17 FILES/CW 23.05.17 FILES/sbornik 15.20 B/Program.cs
+++ ClassWork/CW 23.05.17 FILES/CW 23.05.17 FILES/sbornik 15.20 B/Program.cs
@@ -1,15 +1,28 @@
-﻿using (StreamReader sr = new StreamReader ( "text.txt" ))
+﻿//15.20.Имеется текстовый файл.
+
+//       б) Найти номер самой длинной строки.
+//       Если таких строк несколько, то найти номер одной из них.
+
+using (StreamReader sr = new StreamReader ( "text.txt" ))
 {
     string? line;
-    string max;
+    string max = "";
+    int count = 0;
+    int maxCount = 0;
 
     while (( line = await sr.ReadLineAsync () ) != null)
     {
-        if (true)
-        {
+        count++;
 
+        if (line.Length > max.Length)
+        {
+            max = line;
+            maxCount = count;
         }
     }
+
+    Console.WriteLine(max);
+    Console.WriteLine(maxCount);
 }
 
 Console.ReadKey ();
--- ClassWork/CW 23.05.17 FILES/CW 23.05.17 FILES/sbornik 15.29/Program.cs
+++ ClassWork/CW 23.05.17 FILES/CW 23.05.17 FILES/sbornik 15.29/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿//15.29.Имеется текстовый файл.
+//
+//Все четные строки этого файла записать во второй файл,
+//а нечетные — в третий файл.
+//
+//Порядок следования строк сохраняется.
+
+using (StreamReader sr = new StreamReader("text.txt"))
+{
+    string? line;
+    int count = 0;
+
+    while ((line = await sr.ReadLineAsync()) != null)
+    {
+        count++;
+        line = line + "\n";
+
+        if (count % 2 == 0)
+        {
+            using (StreamWriter sr2 = new StreamWriter("newText1",true))
+            {
+                await sr2.WriteAsync(line);
+            }
+        }
+        else
+        {
+            using (StreamWriter sr3 = new StreamWriter("newText2",true))
+            {
+                await sr3.WriteAsync(line);
+            }
+        }
+    }
+}
