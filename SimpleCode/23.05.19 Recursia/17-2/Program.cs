c6180e91a4a5ffe64b290e98b84aaad61b074e6e
NikSi039 <sidorova9999@internet.ru>
2023-05-19 14:58:23 +0200

    end

---
+++ SimpleCode/23.05.19 Recursia/17-2/Program.cs
@@ -0,0 +1,22 @@
+﻿int myValue = 561;
+int result = Foo(myValue);
+
+Console.WriteLine(result);
+
+Console.ReadKey();
+
+
+
+static int Foo(int value)
+{
+    int result = 0;
+
+    while (value > 0)
+    {
+        result += value % 10;
+        value /= 10;
+    }
+
+    return result;
+}
+
---
+++ SimpleCode/23.05.19 Recursia/17/Program.cs
@@ -0,0 +1,20 @@
+﻿int myValue = 561;
+
+int result = Foo(myValue);
+
+Console.WriteLine(result);
+
+Console.ReadKey();
+
+
+
+static int Foo(int value)
+{
+    if (value < 10)
+    {
+        return value;
+    }
+
+    return value % 10 + Foo(value / 10);
+}
+
