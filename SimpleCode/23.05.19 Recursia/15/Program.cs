64f7d0e166efc49ba607aecf50fd03ccfe5cb22d
NikSi039 <sidorova9999@internet.ru>
2023-05-19 13:36:32 +0200

    SC 23.05.19 Recurcia

---
+++ SimpleCode/23.05.19 Recursia/15/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] myArray = { 5, 3, 2 };
+PrintArray(myArray);
+
+Console.ReadKey();
+
+
+
+static void PrintArray(int[] myArray, int i = 0)
+{
+    if (i < myArray.Length)
+    {
+        Console.WriteLine(myArray[i]);
+
+        PrintArray(myArray, i + 1);
+    }
+}
---
+++ SimpleCode/23.05.19 Recursia/16/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] myArray = { 5, 3, 2 };
+
+int result = Sum(myArray);
+
+Console.WriteLine(result);
+
+Console.ReadKey();
+
+
+
+static int Sum(int[] myArray, int i = 0)
+{
+    if (i >= myArray.Length)
+    {
+        return 0;
+    }
+
+    return myArray[i] + Sum(myArray, i + 1);
+}
