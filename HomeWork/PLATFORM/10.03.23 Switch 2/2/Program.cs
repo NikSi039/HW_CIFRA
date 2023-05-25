69ebfdbb2ae26f4428aa9aa36343dfb02236c141
NikSi039 <sidorova9999@internet.ru>
2023-05-25 18:54:34 +0200

    end

--- HomeWork/PLATFORM/10.03.23 Switch 2/2/Program.cs
+++ HomeWork/PLATFORM/10.03.23 Switch 2/2/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("1 - Chanel\n2 - Dior\n3 - Tom Ford\n4 - Gucci\n5 - Yves Saint Laurent");
 
+Console.Write("\n\n\nВыберите аромат: ");
+byte choose;
+
+if (byte.TryParse(Console.ReadLine(), out choose))
+{
+    switch (choose)
+    {
+        case 1: Console.WriteLine("\nSuper"); break;
+        case 2: Console.WriteLine("\nFantastic"); break;
+        case 3: Console.WriteLine("\nMood"); break;
+        case 4: Console.WriteLine("\nUnbelivable"); break;
+        case 5: Console.WriteLine("\nGood"); break;
+        default: Console.WriteLine("\nВведено некорректное значение"); break;
+    }
+}
+else
+{
+    Console.WriteLine("\nВведено некорректное значение");
+}
+
+Console.ReadKey();
