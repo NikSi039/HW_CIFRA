c64a142887b8cbf8bbe5fc602b7749ded25968ac
NikSi039 <sidorova9999@internet.ru>
2023-05-25 12:12:09 +0200

    Calculator

---
+++ HomeWork/PLATFORM/10.03.23 SWITCH/calc/Program.cs
@@ -0,0 +1,42 @@
+﻿float _number1;
+float _number2;
+float _result = 0;
+int _operation;
+
+Console.Write("Введите первое число: ");
+_number1 = float.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+_number2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("\nВыберете действие: \n\n1 - + \n2 - - \n3 - * \n4 - /");
+_operation = int.Parse(Console.ReadLine());
+
+switch (_operation)
+{
+    case 1:
+        _result = _number1 + _number2;
+        break;
+    case 2:
+        _result = _number1 - _number2;
+        break;
+    case 3:
+        _result = _number1 * _number2;
+        break;
+    case 4:
+        _result = _number1 / _number2;
+        break;
+    default:
+        Console.WriteLine("Vibirete korrektnuu operaciu");
+        break;
+}
+
+Console.WriteLine(_result);
+
+
+
+
+
+
+Console.ReadKey();
+
