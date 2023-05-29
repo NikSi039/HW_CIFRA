d61db06fe83c85c7c9b251f10f69e88f1403f1ac
NikSi039 <sidorova9999@internet.ru>
2023-05-29 22:17:33 +0200

    linq

---
+++ ClassWork/CW 23.05.29 Linq/CW 23.05.29 Linq/CW 23.05.29 Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[]mas = new int[n];
+
+Random random = new();
+
+for (int i = 0; i < n; i++)
+{
+    mas[i] = random.Next(-10,11);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine();
+
+int s = 0;
+
+foreach (int i in mas)
+{
+    s += i;
+}
+
+
+
+Console.ReadKey();
