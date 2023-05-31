f8ee63c9b18bd3f6d2f9351cc31ff073137d5eed
NikSi039 <sidorova9999@internet.ru>
2023-05-31 22:19:05 +0200

    Cw 23.05.31 Algoritm

---
+++ ClassWork/CW 23.05.31 Algoritm/Cw 23.05.31 Algoritm/2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] mas = new int[n];
+
+Random random = new Random();
+
+mas = mas.Select(i => i + random.Next(-10, 11)).ToArray();
+
+foreach (int item in mas)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+
+// Сортировка методом простой вставки
+
+
+
+Console.ReadLine();
---
+++ ClassWork/CW 23.05.31 Algoritm/Cw 23.05.31 Algoritm/Cw 23.05.31 Algoritm/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] mas = new int[n];
+
+Random random = new Random();
+
+mas = mas.Select(i => i + random.Next(-10, 11)).ToArray();
+
+foreach (int item in mas)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+// Алгоритм методом выбора
+
+for (int i = 0; i < mas.Length - 1; i++)
+{
+    int nMin = i;
+    for (int j = i + 1; j < mas.Length; j++)
+    {
+        if (mas[j] < mas[nMin])
+        {
+            nMin = j;
+        }
+    }
+    if (nMin != i)
+    {
+        int c = mas[i];
+        mas[i] = mas[nMin];
+        mas[nMin] = c;
+    }
+    foreach (int item in mas)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+foreach (int item in mas)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+Console.ReadLine(); 
