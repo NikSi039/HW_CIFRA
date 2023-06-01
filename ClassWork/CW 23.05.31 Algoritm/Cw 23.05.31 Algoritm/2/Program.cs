ed67bbb1ca2cedd9ede0b1960d1077b39e1dceb5
NikSi039 <sidorova9999@internet.ru>
2023-06-02 00:10:47 +0200

    Algoritm end

--- ClassWork/CW 23.05.31 Algoritm/Cw 23.05.31 Algoritm/2/Program.cs
+++ ClassWork/CW 23.05.31 Algoritm/Cw 23.05.31 Algoritm/2/Program.cs
@@ -3,19 +3,32 @@ int n = int.Parse(Console.ReadLine());
 
 int[] mas = new int[n];
 
-Random random = new Random();
+Random random = new();
 
 mas = mas.Select(i => i + random.Next(-10, 11)).ToArray();
 
-foreach (int item in mas)
-{
-    Console.Write(item + " ");
-}
+foreach (int item in mas) Console.Write(item + " ");
 Console.WriteLine();
 
 
 // Сортировка методом простой вставки
 
 
+for (int i = 1; i < mas.Length; i++)
+{
+    if (mas[i - 1] > mas[i])
+    {
+        int x = mas[i];
+        int j = i - 1;
+        while (j >= 0 && mas[j] > x)
+        {
+            mas[j + 1] = mas[j];
+            j--;
+        }
+        mas[j + 1] = x;
+    }
+    foreach (int item in mas) Console.Write(item + " ");
+    Console.WriteLine();
+}
 
 Console.ReadLine();
--- ClassWork/CW 23.05.31 Algoritm/Cw 23.05.31 Algoritm/Cw 23.05.31 Algoritm/Program.cs
+++ ClassWork/CW 23.05.31 Algoritm/Cw 23.05.31 Algoritm/Cw 23.05.31 Algoritm/Program.cs
@@ -3,14 +3,11 @@ int n = int.Parse(Console.ReadLine());
 
 int[] mas = new int[n];
 
-Random random = new Random();
+Random random = new();
 
 mas = mas.Select(i => i + random.Next(-10, 11)).ToArray();
 
-foreach (int item in mas)
-{
-    Console.Write(item + " ");
-}
+foreach (int item in mas) Console.Write(item + " ");
 Console.WriteLine();
 
 // Алгоритм методом выбора
@@ -20,10 +17,7 @@ for (int i = 0; i < mas.Length - 1; i++)
     int nMin = i;
     for (int j = i + 1; j < mas.Length; j++)
     {
-        if (mas[j] < mas[nMin])
-        {
-            nMin = j;
-        }
+        if (mas[j] < mas[nMin]) nMin = j;
     }
     if (nMin != i)
     {
@@ -31,17 +25,11 @@ for (int i = 0; i < mas.Length - 1; i++)
         mas[i] = mas[nMin];
         mas[nMin] = c;
     }
-    foreach (int item in mas)
-    {
-        Console.Write(item + " ");
-    }
+    foreach (int item in mas) Console.Write(item + " ");
     Console.WriteLine();
 }
 
-foreach (int item in mas)
-{
-    Console.Write(item + " ");
-}
+foreach (int item in mas) Console.Write(item + " ");
 Console.WriteLine();
 
-Console.ReadLine(); 
+Console.ReadLine();
