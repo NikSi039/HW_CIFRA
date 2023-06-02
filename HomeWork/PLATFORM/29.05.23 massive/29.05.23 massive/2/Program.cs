74cae3c285f7cadf8baf53b4bb10267f98052a2f
NikSi039 <sidorova9999@internet.ru>
2023-06-02 12:50:52 +0200

    hw 29.05.23

---
+++ HomeWork/PLATFORM/29.05.23 massive/29.05.23 massive/2/Program.cs
@@ -0,0 +1,18 @@
+﻿Random random = new();
+
+int[][] numbers = new int[3][];
+numbers[0] = new int[5];
+numbers[1] = new int[10];
+numbers[2] = new int[7];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers[i].Length; j++)
+    {
+        numbers[i][j] = random.Next(1, 11);
+        Console.Write($"{numbers[i][j]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
