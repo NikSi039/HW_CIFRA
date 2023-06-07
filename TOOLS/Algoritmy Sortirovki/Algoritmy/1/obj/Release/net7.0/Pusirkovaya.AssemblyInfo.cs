5bb0434fa730197eeeeff95a3c2b919df69d78c3
NikSi039 <sidorova9999@internet.ru>
2023-06-07 19:41:02 +0200

    add

--- TOOLS/Algoritmy Sortirovki/Algoritmy/1/obj/Release/net7.0/Pusirkovaya.AssemblyInfo.cs
+++ TOOLS/Algoritmy Sortirovki/Algoritmy/1/obj/Release/net7.0/Pusirkovaya.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("vybora")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Pusirkovaya")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("vybora")]
-[assembly: System.Reflection.AssemblyTitleAttribute("vybora")]
+[assembly: System.Reflection.AssemblyProductAttribute("Pusirkovaya")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Pusirkovaya")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
