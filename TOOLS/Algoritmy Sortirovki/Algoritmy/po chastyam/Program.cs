8d8ef276dfac180c071aeee2bdcf63adf70c3c1d
NikSi039 <sidorova9999@internet.ru>
2023-06-07 20:01:35 +0200

    algoritm

---
+++ TOOLS/Algoritmy Sortirovki/Algoritmy/po chastyam/Program.cs
@@ -0,0 +1,45 @@
+﻿// Сортировка по частям
+
+Random r = new();
+
+Console.Write("Введите размер массива:");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] mas = new int[n];
+mas = mas.Select(i => i + r.Next(10, 100)).ToArray();
+
+foreach (int i in mas) Console.Write(i + " ");
+Console.WriteLine();
+
+mas = StoogeSort(mas, 0, mas.Length - 1);
+
+foreach (int i in mas) Console.Write(i + " ");
+
+Console.ReadKey();
+
+
+//END
+
+
+void Swap(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int[] StoogeSort(int[] mas, int startIndex, int endIndex)
+{
+    if (mas[startIndex] > mas[endIndex])
+    {
+        Swap(ref mas[startIndex], ref mas[endIndex]);
+    }
+    if (endIndex -startIndex >1)
+    {
+        int len = (endIndex - startIndex + 1) / 3;
+        StoogeSort(mas, startIndex,endIndex- len);
+        StoogeSort(mas, startIndex + len, endIndex);
+        StoogeSort(mas, startIndex, endIndex - len);
+    }
+    return mas;
+}
