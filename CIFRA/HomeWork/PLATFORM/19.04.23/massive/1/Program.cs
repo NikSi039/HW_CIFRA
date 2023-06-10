bf65c1197c6e3d185b90c9f95666b2304df8fe17
NikSi039 <sidorova9999@internet.ru>
2023-06-10 17:10:37 +0200

    cw 09.06.23

---
+++ CIFRA/HomeWork/PLATFORM/19.04.23/massive/1/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("\n" + Sum());
+
+Console.ReadKey();
+
+// END
+
+double Sum()
+{
+    double firstValue = GetNumber("первое");
+    double secondValue = GetNumber("второе");
+
+    return firstValue + secondValue;
+}
+
+double GetNumber(string massage)
+{
+    bool next = false;
+    double number = 0;
+
+    while (next == false)
+    {
+        Console.Write($"Введите {massage} число: ");
+        if (double.TryParse(Console.ReadLine().Replace(",", "."), out number))
+        {
+            next = true;
+        }
+        else Console.WriteLine("\nВведите корректное значение");
+    }
+    return number;
+}
---
+++ CIFRA/HomeWork/PLATFORM/19.04.23/massive/2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(GetAverage());
+
+Console.ReadKey();
+
+//END
+
+double GetAverage()
+{
+    Random random = new();
+
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-99, 100);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("\n");
+
+    double average = array.Average();
+    return average;
+}
