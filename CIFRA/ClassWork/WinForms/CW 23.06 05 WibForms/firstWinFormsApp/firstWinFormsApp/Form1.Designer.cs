566ea17baa4a1603d96e5db2cae45cdeddd231f1
NikSi039 <sidorova9999@internet.ru>
2023-06-18 19:48:55 +0300

    imac

--- CIFRA/ClassWork/WinForms/CW 23.06 05 WibForms/firstWinFormsApp/firstWinFormsApp/Form1.Designer.cs
+++ CIFRA/ClassWork/WinForms/CW 23.06 05 WibForms/firstWinFormsApp/firstWinFormsApp/Form1.Designer.cs
@@ -28,95 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.tbKurs = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tbDollar = new System.Windows.Forms.TextBox();
-            this.btResult = new System.Windows.Forms.Button();
-            this.lbResult = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            label1 = new Label();
+            tbKurs = new TextBox();
+            label2 = new Label();
+            tbDollar = new TextBox();
+            btResult = new Button();
+            lbResult = new Label();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(91, 58);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Введите курс доллара";
+            label1.AutoSize = true;
+            label1.Location = new Point(280, 36);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(403, 51);
+            label1.TabIndex = 0;
+            label1.Text = "Введите курс доллара";
             // 
             // tbKurs
             // 
-            this.tbKurs.Location = new System.Drawing.Point(91, 105);
-            this.tbKurs.Margin = new System.Windows.Forms.Padding(5);
-            this.tbKurs.Name = "tbKurs";
-            this.tbKurs.Size = new System.Drawing.Size(381, 32);
-            this.tbKurs.TabIndex = 1;
-            this.tbKurs.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbKurs_KeyPress);
+            tbKurs.Location = new Point(289, 115);
+            tbKurs.Margin = new Padding(5);
+            tbKurs.Name = "tbKurs";
+            tbKurs.Size = new Size(381, 57);
+            tbKurs.TabIndex = 1;
+            tbKurs.KeyPress += tbKurs_KeyPress;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(91, 170);
-            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(386, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Введите количество долларов для покупки";
+            label2.AutoSize = true;
+            label2.Location = new Point(210, 177);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(559, 102);
+            label2.TabIndex = 2;
+            label2.Text = "Введите количество долларов \r\nдля покупки";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // tbDollar
             // 
-            this.tbDollar.Location = new System.Drawing.Point(91, 217);
-            this.tbDollar.Margin = new System.Windows.Forms.Padding(5);
-            this.tbDollar.Name = "tbDollar";
-            this.tbDollar.Size = new System.Drawing.Size(381, 32);
-            this.tbDollar.TabIndex = 3;
-            this.tbDollar.TextChanged += new System.EventHandler(this.tbDollar_TextChanged);
-            this.tbDollar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDollar_KeyPress);
+            tbDollar.Location = new Point(280, 303);
+            tbDollar.Margin = new Padding(5);
+            tbDollar.Name = "tbDollar";
+            tbDollar.Size = new Size(381, 57);
+            tbDollar.TabIndex = 3;
+            tbDollar.KeyPress += tbDollar_KeyPress;
             // 
             // btResult
             // 
-            this.btResult.Location = new System.Drawing.Point(214, 303);
-            this.btResult.Margin = new System.Windows.Forms.Padding(5);
-            this.btResult.Name = "btResult";
-            this.btResult.Size = new System.Drawing.Size(118, 38);
-            this.btResult.TabIndex = 4;
-            this.btResult.Text = "Расчитать";
-            this.btResult.UseVisualStyleBackColor = true;
-            this.btResult.Click += new System.EventHandler(this.btResult_Click);
+            btResult.Location = new Point(325, 391);
+            btResult.Margin = new Padding(5);
+            btResult.Name = "btResult";
+            btResult.Size = new Size(283, 103);
+            btResult.TabIndex = 4;
+            btResult.Text = "Расчитать";
+            btResult.UseVisualStyleBackColor = true;
+            btResult.Click += btResult_Click;
             // 
             // lbResult
             // 
-            this.lbResult.ForeColor = System.Drawing.Color.Red;
-            this.lbResult.Location = new System.Drawing.Point(91, 390);
-            this.lbResult.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lbResult.Name = "lbResult";
-            this.lbResult.Size = new System.Drawing.Size(383, 142);
-            this.lbResult.TabIndex = 5;
-            this.lbResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lbResult.ForeColor = Color.Red;
+            lbResult.Location = new Point(267, 526);
+            lbResult.Margin = new Padding(5, 0, 5, 0);
+            lbResult.Name = "lbResult";
+            lbResult.Size = new Size(383, 142);
+            lbResult.TabIndex = 5;
+            lbResult.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 565);
-            this.Controls.Add(this.lbResult);
-            this.Controls.Add(this.btResult);
-            this.Controls.Add(this.tbDollar);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.tbKurs);
-            this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(5);
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Конвертер";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(20F, 50F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(925, 733);
+            Controls.Add(lbResult);
+            Controls.Add(btResult);
+            Controls.Add(tbDollar);
+            Controls.Add(label2);
+            Controls.Add(tbKurs);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(5);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Конвертер";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- CIFRA/ClassWork/WinForms/CW 23.06 05 WibForms/firstWinFormsApp/firstWinFormsApp/Form1.cs
+++ CIFRA/ClassWork/WinForms/CW 23.06 05 WibForms/firstWinFormsApp/firstWinFormsApp/Form1.cs
@@ -7,52 +7,37 @@ namespace firstWinFormsApp
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void btResult_Click(object sender, EventArgs e)
         {
             try
             {
-                decimal kurs = decimal.Parse(tbKurs.Text.Replace(".",","));
+                decimal kurs = decimal.Parse(tbKurs.Text.Replace(".", ","));
                 int dollars = int.Parse(tbDollar.Text);
                 decimal result = kurs * dollars;
-               // lbResult.Text = $"{dollars}$ = {result} рублей";
-               lbResult.Text = dollars + "$=" + result.ToString("C");
+                lbResult.Text = dollars + "$=" + result.ToString("C");
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show(ex.Message);
             }
         }
 
         private void tbKurs_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar>='0' && e.KeyChar<='9')  return;
+            if (e.KeyChar >= '0' && e.KeyChar <= '9') return;
             if (e.KeyChar == '.') e.KeyChar = ',';
             if (e.KeyChar == ',')
             {
-                if (tbKurs.Text.IndexOf(',')!= -1)
-                {
-                    e.Handled = true;
-                }
+                if (tbKurs.Text.IndexOf(',') != -1) e.Handled = true;
                 return;
             }
             if (Char.IsControl(e.KeyChar))
             {
-                if(e.KeyChar == (char)Keys.Enter) tbDollar.Focus();
+                if (e.KeyChar == (char)Keys.Enter) tbDollar.Focus();
                 return;
             }
             e.Handled = true;
-           
-        }
 
-        private void tbDollar_TextChanged(object sender, EventArgs e)
-        {
-           
         }
 
         private void tbDollar_KeyPress(object sender, KeyPressEventArgs e)
@@ -61,10 +46,7 @@ namespace firstWinFormsApp
             if (e.KeyChar == '.') e.KeyChar = ',';
             if (e.KeyChar == ',')
             {
-                if (tbKurs.Text.IndexOf(',') != -1)
-                {
-                    e.Handled = true;
-                }
+                if (tbKurs.Text.IndexOf(',') != -1) e.Handled = true;
                 return;
             }
             if (Char.IsControl(e.KeyChar))
