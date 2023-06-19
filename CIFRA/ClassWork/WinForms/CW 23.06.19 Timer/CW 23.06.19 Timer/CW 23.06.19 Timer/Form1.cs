36e0d3dd4e92b342ca887870d68377d34872e88c
NikSi039 <sidorova9999@internet.ru>
2023-06-19 20:09:53 +0200

    tools sort

---
+++ CIFRA/ClassWork/WinForms/CW 23.06.19 Timer/CW 23.06.19 Timer/CW 23.06.19 Timer/Form1.cs
@@ -0,0 +1,10 @@
+namespace CW_23._06._19_Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TOOLS/C#/Algoritmy Sortirovki/Algoritmy/sliyaniya/Program.cs
@@ -0,0 +1,78 @@
+﻿// Сортировка слиянием
+
+Random random = new();
+
+Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] mas = new int[n];
+mas = mas.Select(i => i + random.Next(10, 100)).ToArray();
+
+foreach (int i in mas) Console.Write(i + " ");
+Console.WriteLine("\n");
+
+mas = MergeSortMas(mas);
+
+foreach (int i in mas) Console.Write(i + " ");
+
+Console.ReadKey();
+
+
+// END
+
+
+int[] MergeSort(int[] mas, int lowIndex, int hightIndex)
+{
+    if (lowIndex < hightIndex)
+    {
+        int middleIndex = (lowIndex + hightIndex) / 2;
+        MergeSort(mas, lowIndex,middleIndex);
+        MergeSort(mas, middleIndex + 1, hightIndex);
+        Merge(mas, lowIndex, middleIndex,hightIndex);
+    }
+    return mas;
+}
+
+int[] MergeSortMas(int[] mas)
+{
+    return MergeSort(mas,0, mas.Length - 1);    
+}
+
+// метод слияния массивов
+void Merge(int[] mas, int lowIndex, int middleIndex, int hightIndex)
+{
+    int left = lowIndex;
+    int right = middleIndex + 1;
+    int[] tempMas = new int[hightIndex - lowIndex + 1];
+    int index = 0;
+
+    while ((left <= middleIndex) && (right <= hightIndex))
+    {
+        if (mas[left] <= mas[right])
+        {
+            tempMas[index] = mas[left];
+            left++;
+        }
+        else
+        {
+            tempMas[index] = mas[right];
+            right++;
+        }
+        index++;
+    }
+
+    for (int i = left; i <= middleIndex; i++)
+    {
+        tempMas[index] = mas[i];
+        index++;
+    }
+    for (int i = right; i <= hightIndex; i++)
+    {
+        tempMas[index] = mas[i];
+        index++;
+    }
+    for (int i = 0; i < tempMas.Length; i++)
+    {
+        mas[lowIndex + i] = tempMas[i];
+    }
+}
