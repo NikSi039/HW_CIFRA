885f9cbfce8c28a2c1883bf6847d75da9ebf06fe
NikSi039 <sidorova9999@internet.ru>
2023-06-26 18:51:59 +0200

    tools gnomiya

---
+++ TOOLS/C#/Algoritmy Sortirovki/Algoritmy/gnomiya/Program.cs
@@ -0,0 +1,56 @@
+﻿// gnomiya
+
+
+Random random = new();
+
+Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] mas = new int[n];
+mas = mas.Select(i => i + random.Next(10, 100)).ToArray();
+
+foreach (int i in mas) Console.Write(i + " ");
+Console.WriteLine("\n");
+
+mas = GnomeSort(mas);
+
+foreach (int i in mas) Console.Write(i + " ");
+
+Console.ReadKey();
+
+
+// END
+
+
+int[] GnomeSort(int[] mas)
+{
+    int index = 1;
+    int nextIndex = index + 1;
+
+    while (index < mas.Length)
+    {
+        if (mas[index - 1] < mas[index])
+        {
+            index = nextIndex;
+            nextIndex++;
+        }
+        else
+        {
+            Swap(ref mas[index - 1], ref mas[index]);
+            index--;
+            if (index == 0)
+            {
+                index = nextIndex;
+                nextIndex++;
+            }
+        }
+    }
+    return mas;
+}
+
+void Swap(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
