c32e33df933209136b5fce0a23eead85d367951d
NikSi039 <sidorova9999@internet.ru>
2023-06-27 20:34:17 +0200

    HW platform

---
+++ CIFRA/HomeWork/PLATFORM/26.06.23 Files/1/1/Program.cs
@@ -0,0 +1,19 @@
+﻿//string path = "/Users/programmirovanie/Desktop/newFolder";
+//Directory.CreateDirectory(path);
+
+Console.Write("Введите путь к файлу: ");
+string userPath = Console.ReadLine()!;
+
+Console.Write("Введите текст: ");
+string text = Console.ReadLine()!;
+
+File.WriteAllText(userPath, text);
+
+Console.WriteLine("Файл успешно записан");
+
+string readText = File.ReadAllText(userPath);
+Console.WriteLine(readText);
+
+Console.ReadKey();
+
+
---
+++ CIFRA/HomeWork/PLATFORM/26.06.23 Files/2/2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите путь к файлу: ");
+string userPath = Console.ReadLine()!;
+
+if (File.Exists(userPath))
+{
+    Console.WriteLine("Файл существует ");
+
+    FileInfo fileInfo = new FileInfo(userPath);
+    Console.WriteLine(fileInfo.Length);
+    Console.WriteLine(fileInfo.LastWriteTime);
+    Console.WriteLine(fileInfo.Extension);
+}
+
+Console.ReadKey();
