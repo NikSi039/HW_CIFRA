51d283f83f2caa998488b454c2dcfcb6bbaa5400
NikSi039 <128488999+NikSi039@users.noreply.github.com>
2023-07-02 11:22:21 +0200

    Add files via upload

---
+++ TOOLS/C#/Algoritmy Sortirovki/Algoritmy/Algoritmy Programmirovaniya/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1 - pusirkovaya 
+// 2 - vybora
+// 3 - prostoy vstavki
+// 4 - sheiker
+// 5 - po chastyam
+// 6 - blinnaya
+// 7 - shella
+// 8 - sluchaynaya
+// 9 - sliyaniya
+// 10 - bistraya
+// 11 - gnomiya
+// 12 - rascheskoy
