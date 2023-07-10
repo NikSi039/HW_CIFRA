925c35a669a230fa07705ededed0bde2be0fe46a
NikSi039 <sidorova9999@internet.ru>
2023-07-10 13:19:22 +0200

    try catch

---
+++ CIFRA/HomeWork/PLATFORM/03.07.23/03.07.23 Try Catch/2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите числа через пробел: ");
+string userInput = Console.ReadLine()!;
+
+string[] userNumbers = userInput.Split(new char[] { ' ' },
+    StringSplitOptions.RemoveEmptyEntries);
+
+int[] numbers = new int[userNumbers.Length];
+
+for (int i = 0; i < userNumbers.Length; i++)
+{
+    try
+    {
+        numbers[i] = int.Parse(userNumbers[i]);
+    }
+    catch (Exception ex)
+    {
+        numbers[i] = 0;
+    }
+}
+
+int sum = numbers.Sum();
+Console.Write("Сумма чисел: " + sum);
+
+Console.ReadKey();
