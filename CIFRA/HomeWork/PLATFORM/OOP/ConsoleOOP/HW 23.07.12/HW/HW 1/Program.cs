7637b666d6f6b75c15bae83aa6cdbbb9be150060
NikSi039 <sidorova9999@internet.ru>
2023-07-13 12:06:06 +0200

    add

---
+++ CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.12/HW/HW 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using HW_1;
+
+UserTime userTime = new UserTime();
+
+Console.Write("Введите часы: ");
+userTime.setHours(insertNumber());
+
+Console.Write("Введите минуты: ");
+userTime.setMinutes(insertNumber());
+
+Console.Write("Введите секунды: ");
+userTime.setSeconds(insertNumber());
+
+Console.WriteLine();
+userTime.printTime();
+
+Console.ReadKey();
+
+
+//END
+
+
+int insertNumber()
+{
+    if (int.TryParse(Console.ReadLine(), out int number))
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение");
+        return 0;
+    }
+}
---
+++ CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.12/HW/HW 1/UserTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace HW_1
+{
+    internal class UserTime
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public int getHours()
+        {
+            return hours;
+        }
+        public int getMinutes()
+        {
+            return minutes;
+        }
+        public int getSeconds()
+        {
+            return seconds;
+        }
+
+        public void setHours(int h)
+        {
+            if (h >= 0 && h < 24) hours = h;
+            else Console.WriteLine("Введено некорректное значение");
+
+        }
+        public void setMinutes(int m)
+        {
+            if (m >= 0 && m < 60) minutes = m;
+            else Console.WriteLine("Введено некорректное значение");
+        }
+        public void setSeconds(int s)
+        {
+            if (s >= 0 && s < 60) seconds = s;
+            else Console.WriteLine("Введено некорректное значение");
+        }
+
+        public void printTime()
+        {
+            Console.WriteLine($"Установленное время: " +
+                $"{getHours()}:{getMinutes()}:{getSeconds()}");
+        }
+    }
+}
+
