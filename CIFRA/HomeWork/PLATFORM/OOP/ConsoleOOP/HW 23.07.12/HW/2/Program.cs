a65ad496f5db4104f23ed8e853d2a5afbf6303b4
NikSi039 <sidorova9999@internet.ru>
2023-07-16 15:01:19 +0200

    add

--- CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.12/HW/2/Program.cs
+++ CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.12/HW/2/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2;
 
+TriangleClass triangle = new TriangleClass();
+
+bool _next = false;
+double a, b, c;
+
+while (_next == false)
+{
+    triangle.SetSideA(GetLenght("первой"));
+    triangle.SetSideB(GetLenght("второй"));
+    triangle.SetSideC(GetLenght("третей"));
+
+    a = triangle.GetSideA();
+    b = triangle.GetSideB();
+    c = triangle.GetSideC();
+
+    if (a + b > c && a + c > b && b + c > a) _next = true;
+    else Console.WriteLine("\nТреугольник с такими длинами сторон " +
+        "не может существовать\n");
+}
+
+Console.WriteLine($"\nПериметр треугольника равен: {triangle.GetPerimetr()}");
+Console.WriteLine($"\nПлощадь треугольника равна: {triangle.GetArea()}");
+
+Console.ReadKey();
+
+
+///END
+
+
+double GetLenght(string message)
+{
+    bool next = false;
+    double lenght = 0;
+
+    while (next == false)
+    {
+        Console.Write($"Введите длину {message} стороны треугольника: ");
+
+        if (double.TryParse(Console.ReadLine()!.Replace(",", "."), out lenght))
+        {
+            if (lenght > 0) next = true;
+            else Console.WriteLine("Длина должна быть больше ноля\n");
+        }
+        else Console.WriteLine("Введено некорректное значение\n");
+    }
+    return lenght;
+}
--- CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.12/HW/2/TriangleClass.cs
+++ CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.12/HW/2/TriangleClass.cs
@@ -1,11 +1,59 @@
 ﻿using System;
 namespace _2
 {
-	public class TriangleClass
-	{
-		public TriangleClass()
-		{
-		}
-	}
+    public class TriangleClass
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public void SetSideA(double a)
+        {
+            if (a > 0)
+            {
+                sideA = a;
+            }
+        }
+        public void SetSideB(double b)
+        {
+            if (b > 0)
+            {
+                sideB = b;
+            }
+        }
+        public void SetSideC(double c)
+        {
+            if (c > 0)
+            {
+                sideC = c;
+            }
+        }
+
+        public double GetSideA()
+        {
+            return sideA;
+        }
+        public double GetSideB()
+        {
+            return sideB;
+        }
+        public double GetSideC()
+        {
+            return sideC;
+        }
+
+        public double GetPerimetr()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        public double GetArea()
+        {
+            double semiPerimetr = (sideA + sideB + sideC) / 2;
+            double p = semiPerimetr;
+
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+    }
 }
 
