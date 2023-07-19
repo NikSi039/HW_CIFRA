0c9688992f89680dcac2c81ed44f49645fb1e52c
NikSi039 <sidorova9999@internet.ru>
2023-07-19 21:30:26 +0200

    add

---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.19/CW 23.07.19 Sstatic Method/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_23._07._19_Sstatic_Method
+{
+    internal class Point
+    {
+        public static int Count { get; set; }
+        public static List<Point> OurPoints = new List<Point>();
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0; 
+        public static double LenghtPoints()
+        {
+            double lenght = 0;
+            if (true)
+            {
+                for (int i = 1; i < OurPoints.Count; i++)
+                {
+                    lenght += Math.Sqrt((OurPoints[i].X - OurPoints[i - 1].X) * (OurPoints[i].X - OurPoints[i - 1].X) + (OurPoints[i].Y - OurPoints[i - 1].Y) * (OurPoints[i].Y - OurPoints[i - 1].Y));
+                }
+            }
+           
+        }
+
+
+    }
+}
---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.19/CW 23.07.19 Sstatic Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using CW_23._07._19_Sstatic_Method;
+using System.Drawing;
+
+Console.Write("Введите количество точек: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int i = 1;
+while (i <= n)
+{
+    Console.Write("Введите координату x: ");
+    double x = double.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Введите координату y: ");
+    double y = double.Parse(Console.ReadLine()!);
+
+    Point point = new Point { X = x, Y = y };
+    Point.OurPoints.Add(point);
+    Point.Count++;
+    i++;
+}
+Console.WriteLine(Point.Count);
+
+Console.ReadKey();
+
