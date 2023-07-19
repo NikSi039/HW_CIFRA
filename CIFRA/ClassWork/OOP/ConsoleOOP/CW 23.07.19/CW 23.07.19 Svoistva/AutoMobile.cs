de0845e606e04a803442037bd6265105abb7b7c1
NikSi039 <sidorova9999@internet.ru>
2023-07-19 20:47:31 +0200

    add

--- CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.19/CW 23.07.19 Svoistva/AutoMobile.cs
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.19/CW 23.07.19 Svoistva/AutoMobile.cs
@@ -8,5 +8,53 @@ namespace CW_23._07._19_Svoistva
 {
     internal class AutoMobile
     {
+        public string Name { get;private set; }
+
+        private double fuel;
+        public double Fuel
+        {
+            get { return fuel; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.fuel = value;
+                }
+            }
+        }
+        private double rashod;
+        public double Rashod
+        {
+            get { return rashod; }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.rashod = value;
+                }
+            }
+        }
+
+        public AutoMobile(string name, double fuel, double rasxod)
+        {
+            Name = name;
+            Fuel = fuel;
+            Rashod = rasxod;
+        }
+
+        public void Go(double km)
+        {
+            fuel -= (km * rashod / 100);
+        }
+
+        public double Total()
+        {
+            return (fuel / rashod) * 100;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Total()}");
+        }
     }
 }
--- CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.19/CW 23.07.19 Svoistva/Program.cs
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.19/CW 23.07.19 Svoistva/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using CW_23._07._19_Svoistva;
+
+AutoMobile auto = new AutoMobile("Mercedes", 50, 12);
+
+Console.WriteLine($"Автомобиль: {auto.Name} поехал");
+//Console.Write("Введите сколько км проехал автомобиль: ");
+//auto.Go(double.Parse(Console.ReadLine()!));
+//Console.WriteLine($"Осталось топлива: {auto.Fuel} литров");
+//Console.WriteLine($"Запас топлива на {auto.Total():F2} км");
+
+int km = 0;
+while (auto.Fuel > 0)
+{
+    auto.Go(km);
+    if (auto.Fuel <= 0)
+    {
+        break;
+    }
+    Console.WriteLine($"Осталось топлива: {auto.Fuel:F1} литров");
+    Console.WriteLine($"Запас топлива на {auto.Total():F2} км");
+    km += 10;
+}
+
+Console.ReadKey();
+
