062fd4558f9654fcbfff750bfa2ecd639d1b7acf
NikSi039 <128488999+NikSi039@users.noreply.github.com>
2023-07-23 12:01:59 +0200

    Add files via upload

---
+++ CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.19/HW 23.07.19/Program.cs
@@ -0,0 +1,9 @@
+﻿using HW_23._07._19;
+
+Turniket turniket = new Turniket();
+
+turniket.IsValid();
+turniket.GetValiditiAndTicketType();
+turniket.TurniketIsOpen();
+
+Console.ReadKey();
---
+++ CIFRA/HomeWork/PLATFORM/OOP/ConsoleOOP/HW 23.07.19/HW 23.07.19/Ticket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace HW_23._07._19
+{
+    public class Ticket
+    {
+        private int ticketNumber;
+        public int TicketNumber
+        {
+            get { return ticketNumber; }
+            set { if (value > 0) this.ticketNumber = value; }
+        }
+
+        private string? ticketType;
+        public string? TicketType { get; set; }
+
+        private DateOnly datePurchase;
+        public DateOnly DatePurchase { get; }
+
+        public Ticket(int ticketNumber, string? ticketType,
+            int year, int month, int day)
+        {
+            TicketNumber = ticketNumber;
+            TicketType = ticketType;
+            datePurchase = new DateOnly(year, month, day);
+            DatePurchase = datePurchase;
+        }
+    }
+}
