bc7a5f4c3ec388dc647c36e8b16a2bd8e8ff3e04
NikSi039 <sidorova9999@internet.ru>
2023-07-24 20:35:30 +0200

    namespace

---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.24/CW Prostranstva Imen/CW Prostranstva Imen/Program.cs
@@ -0,0 +1,13 @@
+﻿using CW_Prostranstva_Imen.Heops;
+using CW_Prostranstva_Imen.Semiramida;
+using CW_Prostranstva_Imen.Zeus;
+
+Piramida p = new Piramida();
+p.Age = 1200;
+p.Height = 200;
+
+Garden g = new Garden();
+g.Area = 18000;
+
+Olimpia o = new Olimpia();
+o.Material = "Глина";
---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.24/CW Prostranstva Imen/CW Prostranstva Imen/Zeus/Olimpia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Prostranstva_Imen.Zeus
+{
+    public class Olimpia
+    {
+        public string? Material { get; set; }
+    }
+}
