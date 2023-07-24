d93b74c05d795f9d3e0c18fb09782f16013deb25
NikSi039 <sidorova9999@internet.ru>
2023-07-24 21:20:02 +0200

    bank

---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.24/NullL/NullL/Schet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Schet
+    {
+        public long Id { get;private set; }
+        public decimal Summa { get; set; }
+
+        public Schet(long id)
+        {
+            Id = id;
+            Summa = 0;
+        }
+
+        public void Add(decimal s)
+        {
+            Summa += s;
+        }
+
+        public void Sub(decimal s)
+        {
+            if (Summa >= s)
+            {
+                Summa -= s;
+            }
+            else
+            {
+                Console.WriteLine("Операция невозможна");
+            }
+        }
+    }
+}
