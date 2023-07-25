d333044134b9bb0b96c2b3d1d9818c3331c63f1b
NikSi039 <sidorova9999@internet.ru>
2023-07-25 21:19:19 +0200

    figures

---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.07.25/Figures/Figures/Program.cs
@@ -0,0 +1,12 @@
+﻿using Figures;
+
+Figure f = new Figure("Figure");
+f.Display();
+
+Rectangle r = new Rectangle("Rectangle", 2, 2, 8, 8);
+r.Display();
+Console.WriteLine();
+Console.WriteLine(r.Area());
+
+RectangleColor rc = new RectangleColor("RectangleColor", 2, 2, 10, 10, 1);
+rc.Display();
