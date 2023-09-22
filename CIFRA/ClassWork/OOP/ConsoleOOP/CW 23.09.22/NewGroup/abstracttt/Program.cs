def1dfcff4152eac188ea1a85f5427b58ed212bb
NikSi039 <sidorova9999@internet.ru>
2023-09-22 20:57:36 +0200

    add

---
+++ CIFRA/ClassWork/OOP/ConsoleOOP/CW 23.09.22/NewGroup/abstracttt/Program.cs
@@ -0,0 +1,50 @@
+﻿Client client = new Client("Alice", 500);
+Employee employee = new Employee("Bob", "Manager");
+
+client.Display();
+employee.Display();
+
+Person person = new Person();
+
+
+
+
+
+
+
+
+abstract class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(Name);
+    }
+}
+
+class Client : Person
+{
+    public int Sum { get; set; }
+    public Client(string name, int sum) : base(name)
+    {
+        Sum = sum;
+    }
+}
+
+class Employee : Person
+{
+    public string Position { get; set; }
+    public Employee (string name,string position) : base (name)
+    {
+        Position = position;
+    }
+}
+
+
+
+
